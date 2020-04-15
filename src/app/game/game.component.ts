0ac042d33df738d8403e2146211d643feac4d003
iKozlinskyi <ikozlinskyi@gmail.com>
2020-04-15 23:15:19 +0300

    Refactor timer

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,18 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {GameState} from '../shared/game-state.enum';
+import {TimerService} from './timer.service';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.scss']
 })
-export class GameComponent implements OnInit {
+export class GameComponent implements OnInit, OnDestroy {
   states = GameState;
   currentState: GameState = this.states.BEFORE_GAME;
   clicks = 0;
-  timerValue = 5;
+  timerValue: number;
+  gameDuration = 3;
+  timerSubscription: Subscription;
 
-  constructor() { }
+  constructor(private timerService: TimerService) { }
 
   ngOnInit(): void {
   }
@@ -22,13 +26,18 @@ export class GameComponent implements OnInit {
   }
 
   startGame() {
-    this.currentState = this.states.PLAYING;
-    const intervalId = setInterval(() => {
-      this.timerValue--;
-      if (this.timerValue === 0) {
-        clearInterval(intervalId);
-        this.currentState = this.states.FINISHED;
-      }
-    }, 1000);
+    this.clicks = 0;
+    this.timerSubscription = this.timerService
+      .startTimer(this.gameDuration)
+      .subscribe(val => {
+        this.currentState = this.states.PLAYING;
+        this.timerValue = val;
+      },
+        null,
+        () => this.currentState = this.states.FINISHED);
+  }
+
+  ngOnDestroy(): void {
+    this.timerSubscription.unsubscribe();
   }
 }
---
+++ src/app/game/timer.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import {interval} from 'rxjs';
+import {map, startWith, take} from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TimerService {
+
+  private timer$ = interval(1000);
+
+  constructor() { }
+
+  startTimer(duration: number) {
+    return this.timer$.pipe(
+      startWith(-1),
+      take(duration + 1),
+      map((v) => (duration - 1) - v)
+    );
+  }
+}
